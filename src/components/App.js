ae57261fb2fdaf0102c840cdce07b7166a989114
Saniya_Shete <shetesaniya22@gmail.com>
2026-03-28 11:26:09 +0530

    importing react

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
+import React from "react";
 import "./../styles/App.css";
-
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import UserDetails from "./UserDetails";
 import UserList from "./UserList";
